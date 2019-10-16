aa0a3d64668f709e8233e651287d43001900de59
huipeng.chu <huipeng.chu@cikers.com>
2019-10-16 17:42:38 +0800

    Header 页面添加

---
+++ src/components/home/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="header">
+    <div class="header-left"><div class="iconfont back-icon">&#xe624;</div></div>
+    <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市/游玩/主题</div>
+    <div class="header-right">城市<span class="iconfont arrow-icon">&#xe64a;</span></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeHeader'
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  /* font-size 86; 根是50px suoyi zhijieceliang / 100 */
+  // @import '../../../assets/styles/varibles.styl'
+  @import '~@/assets/styles/varibles.styl'
+  .header
+    display: flex;
+    line-height: 0.86rem;
+    background: #00bdc4;
+    color: #fff;
+    .header-left
+      width: 0.64rem;
+      float: left;
+      .back-icon
+        text-align: center;
+        font-size: 0.3rem;
+    .header-right
+        width: 1.24rem;
+        float: right;
+        text-align: center;
+        .arrow-icon
+          font-size: .24rem;
+    .header-input
+      flex: 1;
+      height: .64rem;
+      line-height: .64rem;
+      margin-left: .2rem;
+      margin-top: 0.12rem;
+      background: #fff;
+      color: #ccc;
+      padding-left: .2rem;
+</style>
