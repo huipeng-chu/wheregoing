2592582be3bcf50528448d16ba5aa052ac29389e
huipeng.chu <huipeng.chu@cikers.com>
2019-10-17 11:07:00 +0800

    recommend weekend 页面提交

--- src/components/home/components/Icons.vue
+++ src/components/home/components/Icons.vue
@@ -86,10 +86,11 @@ export default {
 <style lang="stylus" rel="stylesheet//stylus" scoped>
   @import '~@/assets/styles/varibles.styl'
   @import '~@/assets/styles/mixins.styl'
-  .icons
-    overflow: hidden;
+  .icons >>> .swiper-container
     height: 0;
     padding-bottom: 50%;
+  .icons
+    margin-top: .1rem;
     .icon
       position: relative;
       overflow: hidden;
